58749a2310b33b549030af7013b544d8df1b5723
TomlongTK <1120170646@qq.com>
2018-12-13 15:44:39 +0800

    fix bug

--- WatchDog.java
+++ WatchDog.java
@@ -76,8 +76,12 @@ public class WatchDog {
                 if (timeout.isCancelled() || future.isDone()) //如果定时任务已取消或业务任务已完成不进行操作
                     return;
 
+                //锁的旧值,当finalDelayTimes=1时,也就是第一次延时的时候,旧值为lockValue,延时之后新值为 lockValue + expireTime
+                //当finalDelayTimes=2时,也就是第二次延时的时候,旧值为lockValue + expireTime ,也就是第一次延时之后设置的值，
+                //以此类推
+                long oldLockValue = lockValue + (finalDelayTimes - 1)*expireTime;
                 //如果任务未完成就延长锁时间
-                boolean delayResult = redisLock.delayTime(lock.lockKey(), expireTime, finalDelayTimes, lockValue);
+                boolean delayResult = redisLock.delayTime(lock.lockKey(), expireTime, finalDelayTimes, oldLockValue);
                 if (!delayResult) {
                     //延时失败,可能原因:因为业务超时被其他锁占用或者值被改变,或者值为null,这种情况将正在执行的任务取消
                     //如果出现这种情况,请确认是否有对锁的key做过set,以及调整@Lock里面delayWight的值以确保在锁超时之前完成延时的动作
